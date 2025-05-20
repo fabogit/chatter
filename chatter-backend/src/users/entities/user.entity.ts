20e302b8267561199255bb4f2a8d2432bbc31030
fabogit <fabo.dev.email@gmail.com>
2025-05-20 17:22:36 +0200

    #7
    server: GraphQL User CRUD Query and Mutations

--- chatter-backend/src/users/entities/user.entity.ts
+++ chatter-backend/src/users/entities/user.entity.ts
@@ -1,7 +1,18 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Field, ObjectType } from '@nestjs/graphql';
+import { AbstractEntity } from '../../common/database/abstract.entity';
 
+// Single model for mongodb and GraphQL
+@Schema({ versionKey: false })
 @ObjectType()
-export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+export class User extends AbstractEntity {
+  @Prop()
+  @Field()
+  email: string;
+
+  // excluded as GraphQL field for security
+  @Prop()
+  password: string;
 }
+
+export const UserSchema = SchemaFactory.createForClass(User);
--- chatter-backend/src/users/users.module.ts
+++ chatter-backend/src/users/users.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../common/database/database.module';
+import { User, UserSchema } from './entities/user.entity';
 import { UsersService } from './users.service';
 import { UsersResolver } from './users.resolver';
+import { UsersRepository } from './users.repository';
 
 @Module({
-  providers: [UsersResolver, UsersService],
+  imports: [
+    DatabaseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema,
+      },
+    ]),
+  ],
+  providers: [UsersResolver, UsersService, UsersRepository],
 })
 export class UsersModule {}
