5b9d085cbec5b4ef48645a04542501805ffed4bf
fabogit <fabo.dev.email@gmail.com>
2025-05-21 15:46:21 +0200

    #10
    server: enabled cors for vite client

--- chatter-backend/src/main.ts
+++ chatter-backend/src/main.ts
@@ -6,9 +6,22 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  // Cors for client url
+  app.enableCors({
+    origin: ['http://localhost:5173'], // Vite's default frontend port
+    credentials: true,
+  });
+
+  // Validation
   app.useGlobalPipes(new ValidationPipe());
+
+  // Pino-logger
   app.useLogger(app.get(Logger));
+
+  // Config
   const config = app.get(ConfigService);
+
   await app.listen(config.getOrThrow('PORT'));
 }
 
